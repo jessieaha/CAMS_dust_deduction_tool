03e5a7540e07c91cc0c7b977ab2eda8db40d296f
idenouden99-jpg <i.denouden99@gmail.com>
2026-02-03 10:21:25 +0100

    Add xarray import to util.py

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import xarray as xr
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.colors as mcolors
